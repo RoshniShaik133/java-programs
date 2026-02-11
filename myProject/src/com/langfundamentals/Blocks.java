c7dee0039ee8afe5b9779b35f9fe8303eaf79e66
shaikasif <shaikasif@localhost>
2026-02-11 11:29:38 +0530

    class

---
+++ myProject/src/com/langfundamentals/Blocks.java
@@ -0,0 +1,19 @@
+package com.langfundamentals;
+
+public class Blocks {
+	static {
+		System.out.println("good morning");
+	}
+	static {
+		System.out.println("hii ");
+	}
+	static {
+		System.out.println("bye friends");
+	}
+
+	public static void main(String[] args) {
+		Blocks b = new Blocks();
+
+	}
+
+}
